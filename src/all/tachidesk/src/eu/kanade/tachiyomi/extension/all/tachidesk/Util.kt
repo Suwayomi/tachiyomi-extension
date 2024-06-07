a5292c8a88b057f3c1e144f5380fac402eb226de
Syer10 <syer10@users.noreply.github.com>
2024-06-06 21:08:03 -0400

    Full GraphQL conversion

--- src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Util.kt
+++ src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Util.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.extension.all.tachidesk
 
 import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
@@ -10,16 +11,16 @@ import rx.Emitter
 import rx.Observable
 import kotlin.coroutines.cancellation.CancellationException
 
-@OptIn(ExperimentalCoroutinesApi::class)
+@OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
 fun <T : Any> Flow<T>.asObservable(
-    backpressureMode: Emitter.BackpressureMode = Emitter.BackpressureMode.NONE
+    backpressureMode: Emitter.BackpressureMode = Emitter.BackpressureMode.NONE,
 ): Observable<T> {
     return Observable.create(
         { emitter ->
             /*
-         * ATOMIC is used here to provide stable behaviour of subscribe+dispose pair even if
-         * asObservable is already invoked from unconfined
-         */
+             * ATOMIC is used here to provide stable behaviour of subscribe+dispose pair even if
+             * asObservable is already invoked from unconfined
+             */
             val job = GlobalScope.launch(Dispatchers.Unconfined, start = CoroutineStart.ATOMIC) {
                 try {
                     collect { emitter.onNext(it) }
@@ -35,6 +36,6 @@ fun <T : Any> Flow<T>.asObservable(
             }
             emitter.setCancellation { job.cancel() }
         },
-        backpressureMode
+        backpressureMode,
     )
 }
