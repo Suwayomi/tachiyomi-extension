746d1bc32dcc48a67328ddd87d2f0d39f763888a
Yip Rui Fung <rf@yrf.me>
2025-07-25 16:38:27 +0800

    Add more sorting options
    1. Latest fetched chapter
    2. Recently read
    3. Unread chapter count
    4. Downloaded chapter count

--- src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Tachidesk.kt
+++ src/all/tachidesk/src/eu/kanade/tachiyomi/extension/all/tachidesk/Tachidesk.kt
@@ -256,6 +256,10 @@ class Tachidesk : ConfigurableSource, UnmeteredSource, HttpSource() {
         "Date added",
         "Total chapters",
         "Latest uploaded chapter",
+        "Latest fetched chapter",
+        "Recently read",
+        "Unread chapters",
+        "Downloaded chapters",
     )
     private val defaultSortByIndex = 0
 
@@ -552,6 +556,10 @@ class Tachidesk : ConfigurableSource, UnmeteredSource, HttpSource() {
                         "Date added" -> searchResults.sortedBy { it.inLibraryAt }
                         "Total chapters" -> searchResults.sortedBy { it.chapters.totalCount }
                         "Latest uploaded chapter" -> searchResults.sortedBy { it.latestUploadedChapter?.uploadDate ?: 0 }
+                        "Latest fetched chapter" -> searchResults.sortedBy { it.latestFetchedChapter?.fetchedAt ?: 0 }
+                        "Recently read" -> searchResults.sortedBy { it.latestReadChapter?.lastReadAt ?: 0 }
+                        "Unread chapters" -> searchResults.sortedBy { it.unreadCount }
+                        "Downloaded chapters" -> searchResults.sortedBy { it.downloadCount }
                         else -> searchResults
                     }
                     if (!sortByAscending) {
